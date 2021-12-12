99e9bee8b066801252ece539db3be10eafe047be
Raymond <38056879+vorsyybl@users.noreply.github.com>
2021-12-12 16:23:26 -0500

    Update main.py

--- heart/main.py
+++ heart/main.py
@@ -2,21 +2,20 @@ import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
 
+#   COLLECTION
 df = pd.read_csv('heart.csv')
-
 data = df.loc[:, ['Age', 'Sex', 'RestingBP', 'Cholesterol', 'HeartDisease']]
-info = data.describe()
 
-#   Possible missing values, impossible to have 0 resting BP, and/or 0 chole?
-
+#   PREPROCESSING
+info = df.describe()
+print(info, '\n')
 min_age = info.loc['min', 'Age']
 max_age = info.loc['max', 'Age']
-max_chol = info.loc['max', 'Cholesterol']
-min_chol = info.loc['min', 'Cholesterol']
 
-print(info)
+print(f'Ages range from {min_age} to {max_age}. \n')
 
-print(f'Ages range from {min_age} to {max_age}.')
-print(f'Cholesterol blood ratio range from {min_chol} to {max_chol}.')
+missing_vals = data[data['Cholesterol'] == 0]
+print(missing_vals.index.values)
 
-# find and ommit missing values
+for missing_val in missing_vals:
+    data.drop()
