b2ca9e27768eefa1b24bb44958db1dcb57fbfe5a
Raymond <38056879+vorsyybl@users.noreply.github.com>
2021-12-13 01:42:27 -0500

    Update main.py

--- heart/main.py
+++ heart/main.py
@@ -1,7 +1,9 @@
 import pandas as pd
+import time
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer as ct
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder as le
 import matplotlib.pyplot as plt
 
 #   COLLECTION
@@ -10,7 +12,9 @@ data = df.loc[:, ['Age', 'Sex', 'RestingBP', 'Cholesterol', 'HeartDisease']]
 
 #   PREPROCESSING
 info = data.describe()
-print(info, '\n')
+info_rows = ['count', 'min', 'max']
+print(info)
+print(info.loc[info_rows, :], '\n')
 
 min_age = info.loc['min', 'Age']
 max_age = info.loc['max', 'Age']
@@ -19,13 +23,14 @@ missing_vals = data[data['Cholesterol'] == 0]
 for missing_val in missing_vals.index.values:
     data.drop(missing_val, axis=0, inplace=True)
 
+info = data.describe()
 count = data.describe().loc['count', 'Age']
+print(info.loc[info_rows, :], '\n')
 
-print(f'There are {count} rows.')
+print(f'There are {count} participants.')
 print(f'Ages range from {min_age} to {max_age}. \n')
 
-info = data.describe()
-print(info, '\n')
-
-#   MINING
+#   MINING PRINT CORRELATION ANALYSIS HERE
+print(data.corr())
+#   ANALYSIS PERFORM UNIVARIANT, COVARIANT, AND MULTIVARIANT UP TO MULTI REGRESSION
 
