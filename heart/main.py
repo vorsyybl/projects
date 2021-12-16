d65d34159075b97ee0247faf7ebe63df752867fb
Raymond <38056879+vorsyybl@users.noreply.github.com>
2021-12-16 12:05:18 -0500

    Update main.py

--- heart/main.py
+++ heart/main.py
@@ -7,29 +7,31 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('heart.csv')
 data = df.loc[:, ['Age', 'Sex', 'RestingBP', 'Cholesterol', 'HeartDisease']]
 
-#   PREPROCESSING EDA
+#   PREPROCESSING
 info = data.describe()
 info_rows = ['count', 'min', 'max']
 num_rows = data.shape[0]
 
-#   LOCATE AND REMOVE OUTLIERS
+#   LOCATE OUTLIERS USING A BOXPLOT
 ax = plt.axes()
 ax.boxplot([data['RestingBP'], data['Cholesterol']], labels=['RestingBP', 'Cholesterol'])
 ax.set_facecolor('grey')
 ax.set_ylabel('VALUE')
 plt.show()
 
+#   REMOVING OUTLIERS
 rbp_out = data[(data['RestingBP'] > 170) | (data['RestingBP'] < 90)].index.values
 chol_out = data[(data['Cholesterol'] > 408) | (data['Cholesterol'] < 85)].index.values
 outliers = (set(rbp_out).union(set(chol_out)))
-
 for row in outliers:
     data.drop(row, axis=0, inplace=True)
+
 num_rows = data.shape[0]
 sex_counts = data['Sex'].value_counts()
 counts = [sex_counts[0], sex_counts[1]]
 gender = [sex_counts.index.values[0], sex_counts.index.values[1]]
 
+#   EDA
 print(f'There are {num_rows} participants, {counts[0]} Male, {counts[1]} female.')
 time.sleep(5)
 
