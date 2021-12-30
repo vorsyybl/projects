4e284969c90b87104e10db976db3b013151efe4b
Raymond <38056879+vorsyybl@users.noreply.github.com>
2021-12-29 22:35:31 -0500

    Update happy.py

--- world_happiness_21/happy.py
+++ world_happiness_21/happy.py
@@ -1,7 +1,24 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df = pd.read_csv('world-happiness-report.csv')
-df2 = pd.read_csv('world-happiness-report-2021.csv')
+#   figure object
+ax = plt.axes()
 
-print(df.info())
-print(df.head())
+#   collect
+df = pd.read_csv('world-happiness-report-2021.csv')
+columns = ['Country name', 'Ladder score', 'Social support', 'Healthy life expectancy', 'Freedom to make life choices',
+           'Generosity', 'Perceptions of corruption']
+
+#   happiest countries according to ladder score
+top_ladder = df[columns].sort_values(by='Ladder score', ascending=False).head(20)
+countries_and_score = top_ladder.loc[:, ['Country name', 'Ladder score']]
+x_bar = countries_and_score.iloc[:, 0].values
+y_bar = countries_and_score.iloc[:, 1].values
+
+ax.barh(x_bar, y_bar)
+ax.set_xlim(6.8, 7.9)
+ax.set_xlabel('Ladder Score')
+ax.set_ylabel('Country')
+plt.figure(figsize=(10, 10))
+plt.show()
+
