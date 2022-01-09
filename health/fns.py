69107d28eae3954fe15dbef2c5c2e2121d2af184
Raymond <38056879+vorsyybl@users.noreply.github.com>
2022-01-09 17:25:13 -0500

    Update fns.py

--- health/fns.py
+++ health/fns.py
@@ -1,5 +1,22 @@
 import tkinter as tk
 
+def step_two(choice, window, menu):
+    if choice == 'Create?':
+        #   Create the menu frame.
+        choice_frame = tk.LabelFrame(window, text='Frame works create')
+        choice_frame.grid(row=1, column=0)
+
+        #   Grid the menu within the menu frame.
+        meal_box = tk.Listbox(choice_frame, cursor='cross', bg='grey', fg='yellow', selectbackground='green',
+                              selectmode='browse', font=('Arial', 13))
+        meal_box.config(border=2, relief='sunken')
+        for meal in menu:
+            meal_box.insert(tk.END, meal)
+
+        meal_box.grid(row=1, column=0)
+    elif choice == 'Update?':
+        pass
+    
 #   Create new file.
 def create():
     #   Check if one has already been created for that day.
@@ -13,8 +30,4 @@ def update():
 def new_win():
     pass
 
-def step_two(choice, window):
-    if choice == 'Create?':
-        pass
-    elif choice == 'Update?':
-        pass
+
