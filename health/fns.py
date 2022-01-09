be72e53b4e84ddf3f70b9758dbb769bfed70e17c
Raymond <38056879+vorsyybl@users.noreply.github.com>
2022-01-09 13:07:48 -0500

    Update fns.py

--- health/fns.py
+++ health/fns.py
@@ -1,15 +1,20 @@
+import tkinter as tk
+
 #   Create new file.
 def create():
     #   Check if one has already been created for that day.
     pass
 
-
 #   Update a chosen file from a list.
 def update():
     pass
 
+#   Confirmation alert.
 def new_win():
     pass
 
-def step_two():
-    pass
+def step_two(choice, window):
+    if choice == 'Create?':
+        pass
+    elif choice == 'Update?':
+        pass
