7692aea4f2a5cb179d1d621e3b019de2da9667c9
Raymond <38056879+vorsyybl@users.noreply.github.com>
2022-01-09 20:00:22 -0500

    Update fns.py

--- health/fns.py
+++ health/fns.py
@@ -1,22 +1,27 @@
 import tkinter as tk
 
+
 def step_two(choice, window, menu):
     if choice == 'Create?':
         #   Create the menu frame.
-        choice_frame = tk.LabelFrame(window, text='Frame works create')
+        choice_frame = tk.LabelFrame(window, text='Menu')
         choice_frame.grid(row=1, column=0)
 
         #   Create the meal box object, populate it, and grid the menu within the menu frame.
         meal_box = tk.Listbox(choice_frame, cursor='cross', bg='grey', fg='yellow', selectbackground='green',
                               selectmode='browse', font=('Arial', 13))
         meal_box.config(border=2, relief='sunken')
-        
         for meal in menu:
             meal_box.insert(tk.END, meal)
 
         meal_box.grid(row=1, column=0)
-        
-        #   
+
+        menu_scrollbar = tk.Scrollbar(window, orient=tk.VERTICAL, command=meal_box.yview)
+        menu_scrollbar.grid(row=1, column=1, sticky='nsw', rowspan=2)
+        meal_box['yscrollcommand'] = menu_scrollbar.set
+        add = tk.Button(text='ADD')
+
+        add.grid(row=2, column=1)
     elif choice == 'Update?':
         pass
 
