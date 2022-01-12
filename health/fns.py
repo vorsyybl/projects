427f885716f02edb790908d8d30578487ad70246
Raymond <38056879+vorsyybl@users.noreply.github.com>
2022-01-12 09:05:08 -0500

    Update fns.py

--- health/fns.py
+++ health/fns.py
@@ -1,15 +1,29 @@
 import tkinter as tk
 import sqlite3 as sql
-import datetime as dt
+import pandas as pd
+import food
 
 
+#   Returns index of selected item.
 def selected_item(box):
     for index in box.curselection():
         return index
 
 
+#   Inserts nutritional data from foods dict for every key into main db.
+def insert_data(new_items, data, db):
+    #   Connect.
+    conn = sql.connect(db)
+    c = conn.cursor()
+
+    #   Access the number of times each item occurs in new_list.
+    value_counts = pd.value_counts(new_items)
+
+    #   Get access to the numbers per key, and multiply each by their respective count.
+    
+
 #   Creates a new table within the main database using the current datetime.
-def create_table(db, data):
+def create_table(new_items, data, db):
     #   Check if one has already been created for that day.
     pass
 
@@ -20,11 +34,12 @@ def create_table(db, data):
     c.execute(f'create table test1 (calories int, protein int, carbs int, fiber int, fat int, cholesterol int, calcium int, iron int, magnesium int, sodium int, zinc int, vitamin_a int, thiamine int, vitamin_e int, riboflavin int, niacin int, vitamin_b6 int, vitamin_c int, vitamin_b12 int, selenium int, sugar int, vitamin_d int)')
 
     #   Populate the new table
+    insert_data(new_items, data, db)
 
 
-def add_item(new_items, items, box, window, db):
+def add_item(new_items, menu, box, window, db):
     #   Add selected item to items to add list
-    new_items.append(items[selected_item(box)])
+    new_items.append(menu[selected_item(box)])
 
     #   Create the menu frame.
     choice_frame = tk.LabelFrame(window, text='Add meals...')
@@ -38,8 +53,10 @@ def add_item(new_items, items, box, window, db):
     for new_item in new_items:
         new_box.insert(tk.END, new_item)
 
+    print(new_items)
+
     #   After adding all items, create the table in db along with data.
-    create_button = tk.Button(text='CREATE', command=lambda: create_table(db, new_items))
+    create_button = tk.Button(text='CREATE', command=lambda: create_table(new_items, food.foods, db))
     create_button.grid(row=2, column=1)
 
 
