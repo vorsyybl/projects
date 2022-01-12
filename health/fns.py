2c0b9e680d926eba925df4859d872d4eee6a4ae4
Raymond <38056879+vorsyybl@users.noreply.github.com>
2022-01-12 01:37:16 -0500

    Update fns.py

--- health/fns.py
+++ health/fns.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import sqlite3 as sql
+import datetime as dt
 
 
 def selected_item(box):
@@ -8,18 +9,23 @@ def selected_item(box):
 
 
 #   Creates a new table within the main database using the current datetime.
-def create(meal_list, table_name):
+def create_table(db, data):
     #   Check if one has already been created for that day.
     pass
+
     #   Connect to main db. Create a new table using datetime as the table name.
-    conn = sql.connect('main.db')
+    conn = sql.connect(db)
     c = conn.cursor()
-    c.execute(f'create table {table_name} (calories int, protein int, carbs int, fiber int, fat int, cholesterol int, calcium int, iron int, magnesium int, sodium int, zinc int, vitamin a int, thiamine int, vitamin e int, riboflavin int, niacin int, vitamin b6 int, vitamin c int, vitamin b12 int, selenium int, sugar int, vitamin d int)')
+    c.execute('drop table if exists test1')
+    c.execute(f'create table test1 (calories int, protein int, carbs int, fiber int, fat int, cholesterol int, calcium int, iron int, magnesium int, sodium int, zinc int, vitamin_a int, thiamine int, vitamin_e int, riboflavin int, niacin int, vitamin_b6 int, vitamin_c int, vitamin_b12 int, selenium int, sugar int, vitamin_d int)')
+
     #   Populate the new table
 
 
-def add_item(new_list, items_list, box, window, table_name):
-    new_list.append(items_list[selected_item(box)])
+def add_item(new_items, items, box, window, db):
+    #   Add selected item to items to add list
+    new_items.append(items[selected_item(box)])
+
     #   Create the menu frame.
     choice_frame = tk.LabelFrame(window, text='Add meals...')
     choice_frame.grid(row=1, column=1)
@@ -29,10 +35,11 @@ def add_item(new_list, items_list, box, window, table_name):
                           selectmode='browse', font=('Arial', 13))
     new_box.config(border=2, relief='sunken')
     new_box.grid(row=1, column=1)
-    for new_item in new_list:
+    for new_item in new_items:
         new_box.insert(tk.END, new_item)
 
-    create_button = tk.Button(text='CREATE', command=lambda: create(new_list, table_name))
+    #   After adding all items, create the table in db along with data.
+    create_button = tk.Button(text='CREATE', command=lambda: create_table(db, new_items))
     create_button.grid(row=2, column=1)
 
 
@@ -41,11 +48,8 @@ def reset(new_items):
     print('Items reset, cleared successfully')
 
 
-def step_two(choice, window, menu, new_items, table_name):
+def step_two(choice, window, menu, new_items, db):
     if choice == 'Create?':
-        #   Create a new table.
-        create(new_items, table_name)
-
         #   Create the menu frame.
         choice_frame = tk.LabelFrame(window, text='Add meals...')
         choice_frame.grid(row=1, column=0)
@@ -64,7 +68,7 @@ def step_two(choice, window, menu, new_items, table_name):
         meal_box['yscrollcommand'] = menu_scrollbar.set
 
         #   Buttons.
-        add_button = tk.Button(text='ADD', command=lambda: add_item(new_items, menu, meal_box, window))
+        add_button = tk.Button(text='ADD', command=lambda: add_item(new_items, menu, meal_box, window, db))
         add_button.grid(row=3, column=0)
         reset_button = tk.Button(text='RESET', command=lambda: reset(new_items))
         reset_button.grid(row=4, column=0)
@@ -76,5 +80,3 @@ def step_two(choice, window, menu, new_items, table_name):
 #   Update a chosen file from a list.
 def update():
     pass
-
-
