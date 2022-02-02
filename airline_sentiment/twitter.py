0203309ab68432fbe524e29f3830822fb075814c
Raymond <38056879+vorsyybl@users.noreply.github.com>
2022-02-02 15:56:34 -0500

    Update twitter.py

--- airline_sentiment/twitter.py
+++ airline_sentiment/twitter.py
@@ -1,1 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 
+#   COLLECTION
+df = pd.read_csv('Tweets.csv')
+
+#   ABSTRACT
+columns = df.columns.values
+info = df.info()
+description = df.describe()
+print(columns, '\n',  info, '\n', description)
+
+#   WRANGLING
+sentiment = df.loc[:, ['airline', 'airline_sentiment']]
+sentiment_proportions = sentiment['airline_sentiment'].value_counts()
+print(sentiment)
+print(sentiment_proportions)
+
+ax = plt.axes()
+ax.pie(sentiment_proportions, labels=['negative', 'neutral', 'positive'])
+plt.show()
